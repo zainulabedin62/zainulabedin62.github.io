90ed5ca0a63b5067d9a7799df1b311f9a1863a78
Zain Ul Abedin <zain@goldevelopers.com>
2021-01-20 16:17:01 +0500

    Updates

---
+++ precache-manifest.3a70cb67b9eb4f47fe8736e6e76d51eb.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "595eca996a2b4716a825e95bb27c206e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5a6efc491eb5abf7fdb",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "17323f9cbe5733644f69",
+    "url": "/static/css/4.19dfc3d0.chunk.css"
+  },
+  {
+    "revision": "6a4f88286d32ab0a3744",
+    "url": "/static/css/main.81c7e264.chunk.css"
+  },
+  {
+    "revision": "e999c29359e215d615af",
+    "url": "/static/js/2.36511077.chunk.js"
+  },
+  {
+    "revision": "d0d8190170b812713050bd2e6e947e19",
+    "url": "/static/js/2.36511077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5a6efc491eb5abf7fdb",
+    "url": "/static/js/3.fccd1f1b.chunk.js"
+  },
+  {
+    "revision": "17323f9cbe5733644f69",
+    "url": "/static/js/4.518ad330.chunk.js"
+  },
+  {
+    "revision": "6a4f88286d32ab0a3744",
+    "url": "/static/js/main.9e04ac34.chunk.js"
+  },
+  {
+    "revision": "ba3abf3fb6cad6dd89e7",
+    "url": "/static/js/runtime-main.3f6813eb.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "9de5316103234711518d2ae4def8be46",
+    "url": "/static/media/cbtcomply.9de53161.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "b087f905318a53fa4f997f26b33ea046",
+    "url": "/static/media/frx.b087f905.png"
+  },
+  {
+    "revision": "ccabfa185eaf33da9b1a4ccb360b26ac",
+    "url": "/static/media/gol.ccabfa18.jpeg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "a8e8397b30facf88e59b05677ace1cb4",
+    "url": "/static/media/numl-logo.a8e8397b.png"
+  },
+  {
+    "revision": "d312046be07b69d1f0877bbdfb40d1cf",
+    "url": "/static/media/pgc.d312046b.png"
+  },
+  {
+    "revision": "245669628973b783883cb601c0782629",
+    "url": "/static/media/profileImage.24566962.jpg"
+  },
+  {
+    "revision": "4c13940910295c9876fadc15dbdd411d",
+    "url": "/static/media/ps-logo-black.4c139409.jpg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
